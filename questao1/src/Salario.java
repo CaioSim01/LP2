c3fc960cd7f37797cbc5c4c429d0d70497e8479d
Caio Simao <caio.ssssimao@gmail.com>
2026-05-14 21:03:36 -0300

    Primeiro commit da lista

---
+++ questao1/src/Salario.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class Salario {
+
+    public static void main (String[] args) {
+        int horas;
+        float salario;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Horas Trabalhadas no mês: ");
+        horas= scanner.nextInt();
+
+        salario= horas*20;
+
+        System.out.println("O Seu Salário é: " + salario);
+
+    }
+}
